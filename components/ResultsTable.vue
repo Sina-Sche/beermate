069aa407f5b10917362a9b5fffb41db26a7f1b58
Sina-Sche <si.scheckenbach@gmail.com>
2021-02-13 12:14:45 +0100

    Add ResultsTable

---
+++ components/ResultsTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <table>
+    <tr>
+      <th>Name of Beer</th>
+      <th>ABV Value</th>
+      <th>First Brewed</th>
+    </tr>
+    <tr>
+      <td>Becks</td>
+      <td>6.5%</td>
+      <td>2001</td>
+    </tr>
+    <tr>
+      <td>Corona</td>
+      <td>8.5%</td>
+      <td>2020</td>
+    </tr>
+    <tr>
+      <td>Karlsberg</td>
+      <td>6.5%</td>
+      <td>1991</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'ResultsTable',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style></style>
