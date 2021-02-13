00f12d9f451aaa9faa804a1c147df13caf5fddc0
Sina-Sche <si.scheckenbach@gmail.com>
2021-02-13 12:14:03 +0100

    Add AppHeader

---
+++ components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to Beermate!</h1>
+    <p>The App to find your perfect beer</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style></style>
