07a666e2712b2669b999d9b9d9b8322504efafa3
Sina-Sche <si.scheckenbach@gmail.com>
2021-02-13 12:14:18 +0100

    Add InputField

---
+++ components/InputField.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label>Type the ABV value</label>
+    <input v-model="abv" placeholder="e.g. 6.5%" type="number" />
+    <p>This much: {{ abv }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputField',
+  data() {
+    return { abv: '' }
+  },
+}
+</script>
+
+<style scoped>
+* {
+  margin-top: 20px;
+}
+</style>
