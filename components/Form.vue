d2440123bad577fd2bc3cdca97f5230c60c14c07
Sina-Sche <si.scheckenbach@gmail.com>
2021-02-13 14:39:02 +0100

    Implement Button and Input in Form Component

---
+++ components/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <form>
+    <div>
+      <label>Type the ABV value</label>
+      <input v-model="abv" placeholder="e.g. 6.5%" type="number" />
+    </div>
+    <button type="submit" @click="submitForm(abv)">
+      🍺 Show me the beers!
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: '',
+  emits: ['getBeers'],
+  data() {
+    return { abv: Number }
+  },
+}
+</script>
+
+<style>
+form {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+}
+input {
+  margin-top: 20px;
+}
+button {
+  cursor: pointer;
+  border: none;
+  padding: 10px;
+  border-radius: 20px;
+  background-color: rgba(212, 195, 157, 0.719);
+  margin: 20px;
+}
+</style>
