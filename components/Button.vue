d06e2707a481a3b395f931362ed92658c42394da
Sina-Sche <si.scheckenbach@gmail.com>
2021-02-13 12:14:31 +0100

    Add Button

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button>Find my beer!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style></style>
